c38190b5e7ced0a06e10b1a169b1616799640746
Nir Fakiro <fakironir@gmail.com>
2024-11-27 09:16:03 +0200

    Init repo

---
+++ server.js
@@ -0,0 +1,39 @@
+import path from 'path'
+import http from 'http'
+import cors from 'cors'
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = express()
+const server = http.createServer(app)
+
+app.use(express.json())
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve('puplic')))
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:3000',
+      'http://localhost:3000',
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+    ],
+  }
+  app.use(cors(corsOptions))
+}
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Moveo App :)')
+})
+
+app.get('/**', (req, res) => {
+  res.sendFile(path.resolve('puplic/index.html'))
+})
+
+const port = process.env.PORT || 3030
+
+server.listen(port, () => {
+  console.log('Server is running on port:' + port)
+})
